520d738fec71e36cdde7d5f3faef44a2cfc9c9f1
Debabrata Das <debabratadas711@gmail.com>
2026-03-24 21:41:43 +0530

    add service

--- week-4_user-directory-app/src/app/services/user.service.ts
+++ week-4_user-directory-app/src/app/services/user.service.ts
@@ -1,8 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../components/users/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  
+  private api = 'https://jsonplaceholder.typicode.com/users';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.api);
+  }
+
+  getUserById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.api}/${id}`);
+  }
 }
